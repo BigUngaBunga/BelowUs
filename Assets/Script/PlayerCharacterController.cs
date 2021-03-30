de690fee8c2ec7bfee8d849da20364eee5109c25
Lgk9583 <dunngen2@live.se>
2021-03-30 02:03:05 +0200

    Added character rotation if running the opposite direction that the character is facing

--- Assets/Script/PlayerCharacterController.cs
+++ Assets/Script/PlayerCharacterController.cs
@@ -15,12 +15,17 @@ public class PlayerCharacterController : NetworkBehaviour
     // Update is called once per frame
     private void Update()
     {
-        // exit from update if this is not the local player
+        // Exit from update if this is not the local player
         if (!isLocalPlayer) return;
 
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed.Value;
 
+        // Turns character
+        if (!Mathf.Approximately(0, movement))
+            transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+
+        // Handles jumping
         if (Input.GetButtonDown("Jump") && Mathf.Abs(CharacterRigidBody.velocity.y) < 0.001)
             CharacterRigidBody.AddForce(new Vector2(0, JumpForce.Value), ForceMode2D.Impulse);
     }
