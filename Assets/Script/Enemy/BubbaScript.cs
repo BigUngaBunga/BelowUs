412cb98c7919dd35d55c3dd26728aade1a51edec
OlofN <Olof_norrlind@hotmail.com>
2021-04-01 16:01:58 +0200

    1

---
+++ Assets/Script/Enemy/BubbaScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbaScript : EnemyBase
+{
+
+    protected override void Start()
+    {
+
+    }
+
+    protected override void Update()
+    {
+      
+    }
+   
+
+    //void UpdateXmovement()
+    //{
+    //    //if (transform.localPosition.x < TargetLocation.x) 
+    //    //{
+    //    //    transform.localPosition = transform.localPosition + new Vector3(movementSpeed, 0);
+    //    //}
+    //    //else if (transform.localPosition.x > TargetLocation.x)
+    //    //{
+    //    //    transform.localPosition = transform.localPosition + new Vector3(-movementSpeed, 0);
+    //    //}
+    //}
+    //void UpdateYmovement()
+    //{
+    //    if (transform.localPosition.x)
+    //}
+}
