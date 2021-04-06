6635332d79d08e912cc7636e159e3b66dd4b2a26
OlofN <Olof_norrlind@hotmail.com>
2021-04-06 21:10:45 +0200

    Added basic enemy
    Needs better movement and rotation but it can patrol and target enemy

--- Assets/Script/Enemy/BubbaScript.cs
+++ Assets/Script/Enemy/BubbaScript.cs
@@ -4,31 +4,84 @@ using UnityEngine;
 
 public class BubbaScript : EnemyBase
 {
+    public Transform TargetLocation;
+    private Rigidbody2D rb;
+    private Vector3 direction;
 
     protected override void Start()
     {
-
+        rb = this.GetComponent<Rigidbody2D>();
+        moveSpeedPatrolling = 5f;
+        moveSpeedChasing = 7f;
+        targetName = "GameObject";
+        currentState = enemyState.Patrolling;
+        CreatePatrolArea();
+        SetTarget();
+        GetNextPatrolPosition();
     }
 
     protected override void Update()
     {
-      
-    }
-   
+        switch (currentState)
+        {
+            case enemyState.Patrolling:
+                UpdateRotationPatrolling();
+                UpdateMovementPatrolling();
+                if (Vector3.Distance(targetGameObject.transform.position, transform.position) < 50f)
+                {
+                    currentState = enemyState.Chasing;
+                }
 
-    //void UpdateXmovement()
-    //{
-    //    //if (transform.localPosition.x < TargetLocation.x) 
-    //    //{
-    //    //    transform.localPosition = transform.localPosition + new Vector3(movementSpeed, 0);
-    //    //}
-    //    //else if (transform.localPosition.x > TargetLocation.x)
-    //    //{
-    //    //    transform.localPosition = transform.localPosition + new Vector3(-movementSpeed, 0);
-    //    //}
-    //}
-    //void UpdateYmovement()
-    //{
-    //    if (transform.localPosition.x)
-    //}
+                break;
+            case enemyState.Chasing:
+                UpdateRotationChasing();
+                UpdateMovementChasing();
+                break;
+        }
+    }
+
+    #region chasing
+
+    protected void UpdateRotationChasing()
+    {
+        direction = targetGameObject.transform.position - transform.position;
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        rb.rotation = -angle;
+        direction.Normalize();
+    }
+    protected void UpdateMovementChasing()
+    {
+        rb.MovePosition((Vector3)transform.position + (direction * moveSpeedChasing * Time.deltaTime));
+    }
+    #endregion chasing
+
+    #region patrolling
+    protected void UpdateRotationPatrolling()
+    {
+        //direction = currentPatrolTarget - transform.position;
+        //float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        ////rb.rotation = ;
+
+        //Vector3 eulerAngleVelocity = new Vector3(0, 0, angle);
+        //Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * 2f);
+        //rb.MoveRotation(rb.rotation * deltaRotation);
+
+        //var rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(direction), 20f * Time.deltaTime);
+        //rb.MoveRotation(rb.rotation * rotation);
+
+
+        
+    }
+
+    protected void UpdateMovementPatrolling()
+    {
+        rb.MovePosition((Vector3)transform.position + (direction * moveSpeedPatrolling * Time.deltaTime));
+
+        if(Vector3.Distance(currentPatrolTarget, transform.position) < 1f)
+        {
+            base.GetNextPatrolPosition();
+        }
+    }
+
+    #endregion patrolling
 }
--- Assets/Script/Enemy/EnemyBase.cs
+++ Assets/Script/Enemy/EnemyBase.cs
@@ -4,21 +4,58 @@ using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
-    public Transform TargetLocation;
-    //protected float movementSpeed;
+    protected enum enemyState
+    {
+        Patrolling,
+        Chasing,
+        Attacking
+    }
 
+    protected string targetName;
+    public float moveSpeedPatrolling;
+    public float moveSpeedChasing;
+    protected float currentMoveSpeed = 0;
+    protected GameObject targetGameObject;
+    protected List<Vector3> patrolPositions = new List<Vector3>();
+    protected Vector3 currentPatrolTarget;
+    private float patrolRange = 5;
+    protected enemyState currentState;
+    
     protected virtual void Start()
     {
-        
+
     }
 
     protected virtual void Update()
     {
 
     }
-    
 
-    //public void UpdateTargetLocation()
-    //{    
-    //}
+
+    protected void SetTarget()
+    {
+        targetGameObject = GameObject.Find(targetName);
+    }
+
+    private float GetRandomPatrolNumber()
+    {
+        return Random.Range(-patrolRange, patrolRange);
+    }
+
+    protected void CreatePatrolArea()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            patrolPositions.Add(new Vector3(GetRandomPatrolNumber(), GetRandomPatrolNumber(), 0));
+        }
+    }
+
+    protected void GetNextPatrolPosition()
+    {
+        currentPatrolTarget = patrolPositions[0];
+        patrolPositions.RemoveAt(0);
+        patrolPositions.Add(new Vector3(GetRandomPatrolNumber(), GetRandomPatrolNumber(), 0));
+    }
 }
+
+
