1fb1a2ff87782adc1a108a6de168f6a2555916c9
Lgk9583 <dunngen2@live.se>
2021-04-19 13:30:13 +0200

    Added leave part of station script

--- Assets/Script/Stations/Station.cs
+++ Assets/Script/Stations/Station.cs
@@ -3,12 +3,24 @@ using UnityEngine;
 public class Station : MonoBehaviour
 {
     [SerializeField] private CameraController controller;
+    [SerializeField] private GameObject leaveButton;
     [SerializeField] [TagSelector] private string playerTag;
     [SerializeField] [TagSelector] private string stationTag;
 
+    private bool isTaken = false;
+
     public void CheckCollision(Collision2D collision)
     {
-        if (collision.collider.CompareTag(playerTag))
+        if (!isTaken && collision.collider.CompareTag(playerTag))
+        {
+            isTaken = true;
             controller.SwitchTarget(stationTag);
+        }
+    }
+
+    public void Leave()
+    {
+        isTaken = false;
+        controller.SwitchTarget(playerTag);
     }
 }
