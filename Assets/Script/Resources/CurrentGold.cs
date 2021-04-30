702dcfb990bfe186d5753c1a3742d3dd97faf006
BigUngaBunga <truls.rockstrom.ystad.nu@gmail.com>
2021-04-30 11:41:27 +0200

    Kopplade djup och pengar i HUD
    Nu uppdateras pengar och djup i  det grafiska användargränssnittet.

--- Assets/Script/Resources/CurrentGold.cs
+++ Assets/Script/Resources/CurrentGold.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace BelowUs
 {
-    public static class CurrentGold
+    public class CurrentGold : MonoBehaviour
     {
         public static int Gold { get => currentGold; set { currentGold = value < 0 ? 0 : value; } }
         private static int currentGold;
